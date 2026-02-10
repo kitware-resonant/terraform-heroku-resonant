60081205b4846fb07b031194417de07143f7bec3
Brian Helba <brian.helba@kitware.com>
2026-02-09 20:33:44 -0500

    Use Ubuntu 24.04 for EC2 workers

--- modules/ec2_worker/main.tf
+++ modules/ec2_worker/main.tf
@@ -38,7 +38,7 @@ data "aws_ami" "ec2_worker_launch_default" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 }
 
